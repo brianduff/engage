fbb736a99c56560b4a0d2df9c8cf695fdf762f87
Brian Duff <cairndubh@gmail.com>
2020-01-07 10:44:15 -0800

    Add support for @ args

--- runtime/src/main/java/org/dubh/engage/CommandlineFlagParser.java
+++ runtime/src/main/java/org/dubh/engage/CommandlineFlagParser.java
@@ -1,15 +1,42 @@
 package org.dubh.engage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 /** Knows how to parse commandline flags. */
 class CommandlineFlagParser {
+  private static final Logger LOGGER = Logger.getLogger(CommandlineFlagParser.class.getName());
+
   /** Processes the given set of commandline flags. */
   Map<String, String> parse(String... args) {
+    Deque<String> argQueue = new ArrayDeque<>(Arrays.asList(args));
+
     Map<String, String> map = new HashMap<>();
     String currentArgName = null;
-    for (String arg : args) {
+    while (!argQueue.isEmpty()) {
+      String arg = argQueue.poll();
+      if (arg.length() > 1 && arg.charAt(0) == '@') {
+        List<String> argsFromFile = readArgsFromFile(arg.substring(1));
+        Collections.reverse(argsFromFile);
+        argsFromFile.forEach(a -> argQueue.addFirst(a));
+        arg = argQueue.poll();
+        // The file might be empty, in which case bail
+        if (arg == null) {
+          break;
+        }
+      }
+
       // Process a flag
       if (arg.startsWith("--") && arg.length() > 2) {
         if (currentArgName != null) {
@@ -38,5 +65,56 @@ class CommandlineFlagParser {
       map.put(currentArgName, "true");
     }
     return map;
+  }
+
+  private static List<String> readArgsFromFile(String filename) {
+    List<String> result = Collections.emptyList();
+    File file = new File(filename);
+    if (!file.isFile()) {
+      LOGGER.warning(String.format("File specified by @-prefixed arg doesn't exist: %s", file));
+      return result;
+    }
+
+    try {
+      List<String> args = new ArrayList<>();
+      String content = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+      // First, split the content into lines.
+      String lines[] = content.split("\\r?\\n");
+      // Now, extract words, possibly quoted, from each line.
+      for (String line : lines) {
+        boolean inQuote = false;
+        StringBuilder currentWord = new StringBuilder();
+        for (int i = 0; i < line.length(); i++) {
+          char c = line.charAt(i);
+          if (c == '"') {
+            if (inQuote) {
+              inQuote = false;
+              args.add(currentWord.toString());
+              currentWord.setLength(0);
+            } else {
+              inQuote = true;
+            }
+          } else if (Character.isWhitespace(c)) {
+            if (!inQuote && currentWord.length() > 0) {
+              args.add(currentWord.toString());
+              currentWord.setLength(0);
+            } else if (inQuote) {
+              currentWord.append(c);
+            }
+          } else {
+            currentWord.append(c);
+          }
+        }
+        if (currentWord.length() > 0) {
+          args.add(currentWord.toString());
+        }
+      }
+
+      return args;
+    } catch (IOException e) {
+      LOGGER.warning(String.format("Failed to read file specified by @-prefixed arg: %s", file));
+      return result;
+
+    }
   }
 }
--- runtime/test/java/org/dubh/engage/CommandlineFlagParserTest.java
+++ runtime/test/java/org/dubh/engage/CommandlineFlagParserTest.java
@@ -3,8 +3,19 @@ package org.dubh.engage;
 import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.Rule;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Map;
 
 public class CommandlineFlagParserTest {
+  @Rule
+  public TemporaryFolder tmp = new TemporaryFolder();
+
   private CommandlineFlagParser parser = new CommandlineFlagParser();
 
   @Test
@@ -31,5 +42,27 @@ public class CommandlineFlagParserTest {
   @Test
   public void parse_withEquals_populatesMap() {
     assertThat(parser.parse("--host=localhost")).containsEntry("host", "localhost");
+  }
+
+  @Test
+  public void parse_withMissingAtArg_populatesMap() {
+    assertThat(parser.parse("--host=localhost", "@missingfile.txt")).containsEntry("host", "localhost");
+  }
+
+  @Test
+  public void parse_withAtArg_populatesMap() throws Exception {
+    String path = writeTempFile("--someKey somevalue --someOtherKey \"value with   whitespace\"\n--anotherValue=true\n--trailing=\"well, well");
+    Map<String, String> args = parser.parse("@" + path, "--simpleArg");
+    assertThat(args).containsEntry("someKey", "somevalue");
+    assertThat(args).containsEntry("someOtherKey", "value with   whitespace");
+    assertThat(args).containsEntry("anotherValue", "true");
+    assertThat(args).containsEntry("simpleArg", "true");
+    assertThat(args).containsEntry("trailing", "well, well");
+  }
+
+  private String writeTempFile(String contents) throws IOException {
+    File tempFile = tmp.newFile("args.txt");
+    Files.write(tempFile.toPath(), contents.getBytes(StandardCharsets.UTF_8));
+    return tempFile.getPath();
   }
 }
